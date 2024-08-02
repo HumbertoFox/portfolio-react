293a24f1a56aef776ea32af80cfbf562a9a9112f
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-02 07:49:41 -0300

    add componente habilidades icons

--- src/components/style/skillsstyle.js
+++ src/components/style/skillsstyle.js
@@ -27,6 +27,7 @@ const DivSectionSkills = styled.div`
 const DivSkills = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     max-width: 400px;
     padding: 40px 20px;
     cursor: default;
